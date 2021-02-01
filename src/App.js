2d300689dbc1a24df60a613b1d320e5b0a7d9d59
schmidtb-de <benny@schmidtb.de>
2021-02-01 16:35:10 +0100

    google sign in and styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Route, Switch } from "react-router-dom"
 import "./App.scss"
 import Homepage from "./pages/homepage/Homepage"
 import ShopPage from "./pages/shop/ShopPage"
 import Header from "./components/header/Header"
 import SignInSignUp from "./pages/sign-in-and-sign-up/SignInSignUp"
+import { auth } from "./firebase/firebase.utils"
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => {
+    let unsubscribeFromAuth = null
+    unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user)
+    })
+    return () => {
+      unsubscribeFromAuth()
+    }
+  }, [])
+
   return (
     <div>
-      <Header />
+      <Header currentUser={currentUser} />
       <Switch>
         <Route exact path="/">
           <Homepage />
--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -44,11 +44,12 @@ function SignIn() {
           handleChange={handleChange}
           required
         />
-
-        <CustomButton type="submit">Sign in</CustomButton>
-        <CustomButton onClick={signInWithGoogle}>
-          Sign in with Google
-        </CustomButton>
+        <div className="buttons">
+          <CustomButton type="submit">Sign in</CustomButton>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            Sign in with Google
+          </CustomButton>
+        </div>
       </form>
     </div>
   )
