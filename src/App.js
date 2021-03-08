3d8cffced52d498649c9aabffa889f4d100816b3
schmidtb-de <benny@schmidtb.de>
2021-03-08 17:09:46 +0100

    storing user data

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import Homepage from "./pages/homepage/Homepage"
 import ShopPage from "./pages/shop/ShopPage"
 import Header from "./components/header/Header"
 import SignInSignUp from "./pages/sign-in-and-sign-up/SignInSignUp"
-import { auth } from "./firebase/firebase.utils"
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null)
 
   useEffect(() => {
     let unsubscribeFromAuth = null
-    unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      setCurrentUser(user)
+    unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          })
+        })
+      } else {
+        setCurrentUser({ currentUser: userAuth })
+      }
     })
     return () => {
       unsubscribeFromAuth()
